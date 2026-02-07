c9412b2256e550dd771f32ebdc7ac9ae89ee6db6
Skylar <skylar.som@gmail.com>
2026-02-07 02:03:06 -0500

    Tests for Category starting

--- src/unit_tests/roll/roll_batch.ts
+++ src/unit_tests/roll/roll_batch.ts
@@ -1,16 +1,27 @@
 import type { Quench } from "@ethaks/fvtt-quench";
 import { registerRollReaderSuite } from "./facets_roll_reader_tests";
 import { registerRollTokenSuite } from "./token_tests";
+import { registerRollValueCategorySuite } from "./roll_value_category";
 
 export function registerRollBatch(quench: Quench) {
     quench.registerBatch(
-        "facets.roll",
+        "facets.roll.parse",
         (context) => {
             registerRollReaderSuite(context);
             registerRollTokenSuite(context);
         },
         {
-            displayName: "Facets: Rolling Tests",
+            displayName: "Facets: Roll Parsing Tests",
         },
     );
+
+    quench.registerBatch(
+        "facets.roll.rolling",
+        (context) => {
+            registerRollValueCategorySuite(context);
+        },
+        {
+            displayName: "Facets Rolling Tests"
+        }
+    )
 }
---
+++ src/unit_tests/roll/roll_value_category.ts
@@ -0,0 +1,72 @@
+import type { QuenchBatchContext } from "@ethaks/fvtt-quench";
+import { RegularDiceValueCategory } from "../../module/roll/dice/regular/regular_dice_category";
+import { DiceRollTokenProvider } from "../../module/roll/token/dice_token";
+import { ErrorToken, SuggestionToken } from "../../module/roll/token/roll_token";
+import { RollValue } from "../../module/roll/value/roll_value";
+
+export function registerRollValueCategorySuite(context: QuenchBatchContext) {
+    regularRollCategory(context);
+}
+
+function regularRollCategory(context: QuenchBatchContext) {
+    const { describe, it, expect, assert } = context;
+
+    describe("Regular Roll Value Category", function () {
+        it("It should keep all values if less than default kept value of 2", async function () {
+            let rolled = await roll(["ad6"], assert);
+
+            let picked = RegularDiceValueCategory.INSTANCE.pickValues(rolled);
+
+            expect(picked).to.have.lengthOf(1);
+        });
+
+        it("It should keep only keep the two highest 2 positive values even there are more", async function () {
+            let rolled = await roll(["ad6", "ad8", "ad10"], assert);
+
+            let picked = RegularDiceValueCategory.INSTANCE.pickValues(rolled);
+
+            expect(picked).to.have.lengthOf(2);
+
+            let pickedValues = picked.map((pick) => pick.value());
+            expect(pickedValues).to.eqls([10, 8]);
+
+                        let total = pickedValues.reduce((sum, pickedValue) => sum + pickedValue, 0);
+                        expect(total).to.eq(18);
+        });
+
+        it("It should keep the two highest positive and two highest negative", async function () {
+            let rolled = await roll(["ad6", "ad8", "ad10", "ad12", "acd6", "acd8", "acd10"], assert);
+
+            let picked = RegularDiceValueCategory.INSTANCE.pickValues(rolled);
+
+            expect(picked).to.have.lengthOf(4);
+
+            let pickedValues = picked.map((pick) => pick.value());
+            expect(pickedValues).to.eqls([12, 10, -10, -8]);
+
+            let total = pickedValues.reduce((sum, pickedValue) => sum + pickedValue, 0)
+            expect(total).to.eq(4)
+        });
+    });
+}
+
+async function roll(dice: Array<string>, assert: Chai.AssertStatic): Promise<Array<RollValue>> {
+    let rollValues: Array<RollValue> = dice
+        .map((dice) => {
+            let token = DiceRollTokenProvider.INSTANCE.provide(dice, false, {});
+            if (token instanceof ErrorToken) {
+                assert.fail("Found Error Token: " + token.error);
+            } else if (token instanceof SuggestionToken) {
+                assert.fail("Found Suggestion Token: " + JSON.stringify(token.suggestions));
+            } else if (token == null) {
+                assert.fail("Failed to find token");
+            }
+            return token;
+        })
+        .flatMap((token) => {
+            return token.provide();
+        });
+
+    let promises = rollValues.map((rollValue) => rollValue.evaluate());
+    return Promise.all(promises).then((_) => rollValues);
+}
