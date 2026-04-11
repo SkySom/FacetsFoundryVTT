bcc5a5aa26bd69ed2b56300e05cb42d83b249f90
Skylar <skylar.som@gmail.com>
2026-04-10 22:18:55 -0400

    Add Adjectives to match missing morale/dispirits

--- src/unit_tests/util/localize.ts
+++ src/unit_tests/util/localize.ts
@@ -11,4 +11,22 @@ export function registerLocaleSuite(context: QuenchBatchContext) {
             expect(localized).to.eqls("A Message");
         });
     });
+
+    describe("Added Adjectives", function() {
+        it("Should localize them", function() {
+            expect(game?.i18n?.localize(CONFIG.Token.adjectivesPrefix + ".morose")).to.eqls("Morose")
+        });
+        it("Should include the added ones", function() {
+
+            const translations = game.i18n?.translations;
+            expect(translations).to.not.eq(undefined)
+            if (translations) {
+                const adjectives = foundry.utils.getProperty(game.i18n.translations, CONFIG.Token.adjectivesPrefix);
+
+                expect(adjectives).to.contain.keys("morose");
+                expect(adjectives).to.contain.keys("unhelpful");
+            }
+        });
+    })
+
 }
